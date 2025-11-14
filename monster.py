a8c1167be79868e9324d85e83aa3181cd37445a9
currymaster-blip <jonatan.aronsson@skola.taby.se>
2025-11-14 10:20:49 +0000

    monster

--- monster.py
+++ monster.py
@@ -4,11 +4,12 @@ class Monster():
         self.dmg = dmg
         self.name = name
         self.lvl = lvl
+    
 
-def nivå():
-    lvl += nivå
 
-Monster_list = [    golem = Monster("Golem" , 100, 20, 1)
-    goblin = Monster("Goblin", 25, 5, 1)]
+golem = Monster("Golem" , 100, 20, 1)
+goblin = Monster("Goblin", 25, 5, 1)
 
 print(f"Ditt monster har {golem.hp} hp")
+
+
